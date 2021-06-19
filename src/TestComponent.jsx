e6b77fe5a923c7aeab7b85138fd212df51996054
Oleh <orenkole@gmail.com>
2021-06-19 16:08:53 +0300

    8: Add dark mode to your site (theming). Created switcher between light/dark modes

--- src/TestComponent.jsx
+++ src/TestComponent.jsx
@@ -1,24 +1,45 @@
+import React from 'react'
+import { useState } from 'react';
 import {
   Button,
   Grid,
   Paper,
+  Switch,
   Typography,
 } from '@material-ui/core'
-import React from 'react'
+import {
+  ThemeProvider,
+  createMuiTheme,
+} from '@material-ui/core/styles';
+
+
 
 function TestComponent() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const theme = createMuiTheme({
+    palette: {
+      type: darkMode ? "dark" : "light"
+    }
+  })
   return (
-    <Paper style={{ height: "100vh" }}>
-      <Grid cointaner direction="column">
-        <Typography variant="h1">This is my App</Typography>
-        <Button variant="contained" color="primary">
-          This is a button
+    <ThemeProvider theme={theme} >
+      <Paper style={{ height: "100vh" }}>
+        <Grid cointaner direction="column">
+          <Typography variant="h1">This is my App</Typography>
+          <Button variant="contained" color="primary">
+            This is a button
         </Button>
-        <Button variant="contained" color="primary">
-          This is another button
+          <Button variant="contained" color="secondary">
+            This is another button
         </Button>
-      </Grid>
-    </Paper>
+          <Switch
+            checked={darkMode}
+            onChange={() => (setDarkMode(!darkMode))}
+          />
+        </Grid>
+      </Paper>
+    </ThemeProvider>
   )
 }
 
