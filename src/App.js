d7192732379ca2d1dfaae3bdf61f849d6d367bbe
Oleh <orenkole@gmail.com>
2021-06-19 11:17:57 +0300

    2. Actually coding a UX desing: make layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+
 function App() {
   return (
     <div className="App">
-      
+      <Grid cointainer direction="column">
+        {/* Header */}
+        <Grid item>
+        </Grid>
+        {/* Content */}
+        <Grid item container>
+          {/* Space left */}
+          <Grid xs={0} sm={2} />
+          {/* Actual content */}
+          <Grid sm={8}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          </Grid>
+          {/* Space right */}
+          <Grid xs={0} sm={2} />
+        </Grid>
+      </Grid>
     </div>
   );
 }
