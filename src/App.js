2ff2620a50e6f2095863fcc936344c3c81cd7f1e
Oleh <orenkole@gmail.com>
2021-06-19 11:03:27 +0300

    2. Actually coding a UX desing: install core, icons, styles

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core';
-
-const useStyles = makeStyles({
-  helloThereStyle: {
-    fontStyle: 'oblique',
-    color: 'red',
-  },
-  buttonStyles: {
-    color: 'green',
-    border: 0,
-  },
-})
-
 function App() {
-  const classes = useStyles();
   return (
     <div className="App">
-      <Typography
-        className={classes.helloThereStyle}
-        variant="h1"
-        color="primary"
-      >
-        Hello there
-      </Typography>
-      <Button color="secondary">My first button</Button>
+      
     </div>
   );
 }
