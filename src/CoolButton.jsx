67e07c98b87195740316e5c9ccd06f8aab650f0b
Oleh <orenkole@gmail.com>
2021-06-19 13:27:54 +0300

    3: makeStyles. Style depend on theme

--- src/CoolButton.jsx
+++ src/CoolButton.jsx
@@ -4,15 +4,24 @@ import {
 } from '@material-ui/core'
 import React from 'react'
 
-const useStyles = makeStyles({
-  buttonStyles: props => ({
-    color: props.cool ? "blue" : "red",
-    backgroundColor: props.cool ? "orange" : "yellow",
-  })
-});
+// const useStyles = makeStyles({
+//   buttonStyles: props => ({
+//     color: props.cool ? "blue" : "red",
+//     backgroundColor: props.cool ? "orange" : "yellow",
+//   })
+// });
+
+const useStyles = makeStyles(theme => ({
+  buttonStyles: {
+    color: "red",
+    [theme.breakpoints.up("sm")]: {
+      color: "blue",
+    }
+  }
+}))
 
 function CoolButton(props) {
-  const classes = useStyles(props);
+  const classes = useStyles();
   return (
     <Button className={classes.buttonStyles}>
       The button
