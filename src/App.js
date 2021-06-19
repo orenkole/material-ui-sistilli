5274815314493340f10820e6c272b0b8c1cc0a29
Oleh <orenkole@gmail.com>
2021-06-19 10:46:23 +0300

    1. Introduction: use makeStyles() and useStyles()

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  helloThereStyle: {
+    fontStyle: 'oblique',
+    color: red,
+  }
+})
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
-      <Typography variant="h1" color="primary">
+      <Typography
+        className={classes.helloThereStyle}
+        variant="h1"
+        color="primary"
+      >
         Hello there
       </Typography>
       <Button color="secondary">My first button</Button>
