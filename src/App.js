d4b6247570ca8c049f9190bda1b9a0f255d79480
Oleh <orenkole@gmail.com>
2021-06-21 11:48:49 +0300

    14. Creating a Pokedex (part 1). Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
 import React from "react";
-import Grid from "@material-ui/core/Grid";
-import Header from "./Header";
-import Content from "./Content";
 
 function App() {
   return (
     <div className="App">
-      <Grid container direction="column">
-        {/* Header */}
-        <Grid item>
-          <Header />
-        </Grid>
-        {/* Content */}
-        <Grid item container>
-          {/* Space left */}
-          <Grid xs={false} sm={2} />
-          {/* Actual content */}
-          <Grid xs={12} sm={8}>
-            <Content />
-          </Grid>
-          {/* Space right */}
-          <Grid xs={false} sm={2} />
-        </Grid>
-      </Grid>
+
     </div>
   );
 }
--- src/TestComponent.jsx
+++ src/TestComponent.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react';
 import {
   Button,
+  Card,
   Grid,
   Paper,
   Switch,
@@ -41,6 +42,7 @@ function TestComponent() {
         </Grid>
       </Paper>
     </ThemeProvider>
+
   )
 }
 
